b24acc70d38f97902c401477ba91e0260dae0344
Nagaku <45015676+Nagaku@users.noreply.github.com>
2019-11-07 21:51:03 +0700

    Add files via upload

---
+++ IF1-10118008-Latihan56-UmurBarangAntik/src/if1/pkg10118008/latihan55/umurbarangantik/BarangAntik.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan55.umurbarangantik;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BarangAntik {
+	protected int umur;
+
+
+	
+	public BarangAntik(int umur){
+		this.umur = umur;
+	}	
+
+	void tampilUmur(){
+		System.out.println("Umur barang antik ini adalah : "+this.umur);
+	}
+}
---
+++ IF1-10118008-Latihan56-UmurBarangAntik/src/if1/pkg10118008/latihan55/umurbarangantik/IF110118008Latihan55UmurBarangAntik.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118008.latihan55.umurbarangantik;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author ASUS
+ */
+public class IF110118008Latihan55UmurBarangAntik {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		Radio rd = new Radio(234);
+		rd.setName("Radio AM");
+		System.out.println("Nama Barang Antik : " + rd.getName());
+		rd.tampilUmur();
+	}
+	
+}
